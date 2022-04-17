614c130c3324eb6952124b6056546acaf70aff81
Sergio <webdev.slara@gmail.com>
2022-04-17 22:28:15 +0200

    created VideoDetail component, added nested callback to get the video the user is clicking

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 //videos have been passed as a prop from the parent component and used to display the thumbnail and title
-const VideoItem = ({video}) => {
+const VideoItem = ({video, onVideoSelect}) => {
     console.log(video)
     return (
-        <div className="video-item item">
+        <div onClick={() => onVideoSelect(video)} className="video-item item">
             <img className="ui image" src={video.snippet.thumbnails.medium.url} alt={video.id.kind}/>
             <div className="content">
                 <div className="header">{video.snippet.title}</div>
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -4,10 +4,10 @@ import VideoItem from './VideoItem';
 
 //retrieving the videos props from the parent component we return a VideoItem component for each video
 
-const VideoList = ({videos}) => {
+const VideoList = ({videos, onVideoSelect}) => {
 
     const renderedList = videos.map((video) => {
-        return <VideoItem video={video}/>
+        return <VideoItem onVideoSelect={onVideoSelect}video={video}/>
     })
     return <div className="ui relaxed divided list">{renderedList}</div>
 }
