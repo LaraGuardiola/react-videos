1419bc0be69495c6b0081a5022fe6d590cb3e060
Sergio <webdev.slara@gmail.com>
2022-04-17 22:08:10 +0200

    added styling with semantic ui

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -4,9 +4,11 @@ import React from 'react';
 const VideoItem = ({video}) => {
     console.log(video)
     return (
-        <div>
-            <img src={video.snippet.thumbnails.medium.url} alt={video.id.kind}/>
-            {video.snippet.title}
+        <div className="video-item item">
+            <img className="ui image" src={video.snippet.thumbnails.medium.url} alt={video.id.kind}/>
+            <div className="content">
+                <div className="header">{video.snippet.title}</div>
+            </div>
         </div>
     )
 }
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,3 +1,4 @@
+import './VideoItem.css';
 import React from 'react';
 import VideoItem from './VideoItem';
 
@@ -8,7 +9,7 @@ const VideoList = ({videos}) => {
     const renderedList = videos.map((video) => {
         return <VideoItem video={video}/>
     })
-    return <div>{renderedList}</div>
+    return <div className="ui relaxed divided list">{renderedList}</div>
 }
 
 export default VideoList;
